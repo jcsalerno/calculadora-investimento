df8170b8c4960f5b658f6fcaf5cb8e376b910d32
Julio Cesar <contato.jcsalerno@gmail.com>
2023-12-14 00:54:44 -0300

    feat: calculation of projections part 1

---
+++ src/investmentGoals.js
@@ -0,0 +1,12 @@
+function convertToMonthlyReturnRate(yearReturnRate) {
+  return yearReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "monthly",
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = "monthly"
+) {}
