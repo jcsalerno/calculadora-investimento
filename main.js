8a17c96ca3ead7079866d302007ec910bc217e9b
Julio Cesar <contato.jcsalerno@gmail.com>
2023-12-17 21:58:18 -0300

    feat: change event, and error detection intelligence

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 import { generateReturnsArray } from "./src/investmentGoals";
 
-const calculateButton = document.getElementById("investment-form");
+const form = document.getElementById("investment-form");
 
 function renderProgression(evt) {
   evt.preventDefault();
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const textRate = Number(document.getElementById("tax-rate").value);
+  const textRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -25,6 +29,31 @@ function renderProgression(evt) {
     returnRatePeriod
   );
   console.log(returnsArray);
+}
+
+function validateInput(evt) {
+  if (evt.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(",", ".");
+
+  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+    const errorTextElement = document.createElement("p");
+    errorTextElement.classList.add("text-red-500");
+    errorTextElement.innerText = "Insira um valor numérico e maior que zero";
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  }
+}
+
+for (const formElement of form.elements) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("id")) {
+    formElement.addEventListener("change", validateInput);
+  }
 }
 
 form.addEventListener("submit", renderProgression);
